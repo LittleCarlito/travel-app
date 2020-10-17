5e488ce7b1f9bc0346cdce5265a3dd03ca8bb52b
LittleCarlito <59811183+LittleCarlito@users.noreply.github.com>
2020-10-16 23:55:07 -0500

    Using angular-boostrap demo

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('travel-app app is running!');
+    expect(page.getTitleText()).toEqual('angular-bootstrap-demo app is running!');
   });
 
   afterEach(async () => {
