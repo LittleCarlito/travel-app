137096aca9411f332e01c42f27ed78606decb353
LittleCarlito <59811183+LittleCarlito@users.noreply.github.com>
2020-10-28 15:30:36 -0500

    For homework 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { TripPlannerComponent } from './trip-planner/trip-planner.component';
+import {  AboutComponent } from './about/about.component';
 
 const routes: Routes = [
-  {path:  "", pathMatch:  "full",redirectTo:  "home"},
-  {path: "home", component: HomeComponent},
-  {path: "trip-planner", component: TripPlannerComponent}
+  {path:  '', pathMatch:  'full',redirectTo:  'about'},
+  {path: 'trip-planner', component: TripPlannerComponent},
+  {path: 'about', component: AboutComponent}
 ];
 
 @NgModule({
